6b03a2a4c46a83f9fc6bd0e57c9e447c82175196
Fernandas7 <maria.rodriguez@epn.edu.ec>
2026-06-23 22:01:32 -0500

    fix: MainAppa.java - mover main dentro de la clase y agregar package

--- Semana12/navegacion/src/main/java/com/example/navegacion/MainAppa.java
+++ Semana12/navegacion/src/main/java/com/example/navegacion/MainAppa.java
@@ -1,22 +1,22 @@
+package com.example.navegacion;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class MainApp extends Application {
+public class MainAppa extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("Login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainAppa.class.getResource("Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setScene(scene);
         stage.setTitle("LOGIN");
         stage.show();
     }
 
-}
-
-// 👇 Este método va aquí, dentro de la clase
-public static void main(String[] args) {
-
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
